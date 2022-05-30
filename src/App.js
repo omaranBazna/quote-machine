5f0d1c2e068e212f033daa643956442dfc97a924
omaranBazna <omran4d@gmail.com>
2022-05-30 16:18:10 -0400

    finish styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import {useState,useEffect} from 'react';
 function App() {
 const [quote,setQuote]=useState(0);
-
+const [bg_color,setBgColor]=useState(0);
 
 useEffect(() => {
   const interval = setInterval(() => {
     setQuote(old=>old+1)
+    setBgColor(old=>old+1)
   }, 4000);
   return () => clearInterval(interval);
 }, []);
@@ -18,24 +19,30 @@ var quotes = [
   ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
   ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"]
 ]
+const colors=[
+  'color1',
+  'color2',
+   'color3',
+ 'color4',
+  'colo5'
+]
   return (
-    <div className="App">
-      <div id="quote-box">
+    <div className={["App",colors[bg_color%(colors.length)]].join(' ')}>
+      <div id="quote-box" className={colors[(bg_color+2)%(colors.length)]}>
         <div id="text">
           {`"${quotes[quote%(quotes.length)][0]}"`}
+       
         </div>
-        <div className="btns">
-        <button id="new-quote" onClick={()=>{setQuote(Math.floor(Math.random() * quotes.length));}}>
-            new quote
-        </button>
         <p id="author">
         {quotes[quote%(quotes.length)][1]}
         </p>
-       
+        <button id="new-quote" onClick={()=>{setQuote(Math.floor(Math.random() * quotes.length));setBgColor(Math.floor(Math.random() *colors.length))}}>
+            new quote
+        </button>
         <a id="tweet-quote">
 
         </a>
-        </div>
+        
       </div>
     </div>
   );
