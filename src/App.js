bd18491f4560e4b108ce5b77263d4dacf930e3a2
omaranBazna <omran4d@gmail.com>
2022-05-30 00:06:57 -0400

    add useEffect ,useState methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState,useEffect} from 'react';
 function App() {
+const [quote,setQuote]=useState(0);
+useEffect(()=>{
+setInterval(()=>{
+setQuote((old)=>{old+1})
+},1000);
+},[quote])
+  const quotes=[
+   "you are stupid",
+   "you are not worthy",
+   "you can not do it"
+  ]
   return (
     <div className="App">
       <div id="quote-box">
         <div id="text">
-
+          {quotes[quote%(quotes.length)]}
         </div>
         <div id="author">
 
@@ -21,7 +32,7 @@ function App() {
 
         </p>
         <h1 id="author">
-          
+
         </h1>
       </div>
     </div>
