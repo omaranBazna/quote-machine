4eada9afd775eced19b219548de7fa043443fdce
omaranBazna <omran4d@gmail.com>
2022-05-29 23:57:04 -0400

    add authors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div id="quote-box">
+        <div id="text">
+
+        </div>
+        <div id="author">
+
+        </div>
+        <button id="new-quote">
+
+        </button>
+        <a id="tweet-quote">
+
+        </a>
+        <p id="text">
+
+        </p>
+        <h1 id="author">
+          
+        </h1>
+      </div>
     </div>
   );
 }
